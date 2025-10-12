fda7f62e9038ed5bd58196aaf461631cbf1500ed
siddhant agrawal <siddhant-agrawal01>
2025-10-13 00:57:57 +0530

    added the schema basic

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
-import connectDB from "./db";
+import connectDB from "./db.js";
+import dotenv from "dotenv";
 
+
+dotenv.config()
 const PORT = process.env.PORT;
-const app = expres();
+const app = express();
 app.use(cors());
 
 app.use(express.json());
@@ -13,3 +16,4 @@ connectDB();
 app.listen(PORT, () => {
   console.log("connected to server");
 });
+
