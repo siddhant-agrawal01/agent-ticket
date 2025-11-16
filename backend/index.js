f7281a49bfe2acd237dfa1e6845ef37b68434107
siddhant agrawal <siddhant-agrawal01>
2025-11-16 19:36:52 +0530

    added the ticket routes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,15 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./db.js";
 import dotenv from "dotenv";
+
 import userRoutes from "./routes/user.js";
+
+import mongoose from "mongoose";
+import { serve } from "inngest/express";
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -12,6 +20,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
+
 connectDB();
 
 app.listen(PORT, () => {
