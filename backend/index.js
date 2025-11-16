af0aa89059d6f502794565bab6f4c804095739cd
siddhant agrawal <siddhant-agrawal01>
2025-11-16 15:18:19 +0530

    added he ai agent prompt and model functionalities

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,18 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./db.js";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.js";
 
-
-dotenv.config()
+dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 
+app.use("/api/auth", userRoutes);
 connectDB();
 
 app.listen(PORT, () => {
   console.log("connected to server");
 });
-
